3fd753d418a861e29696bc469ad69693c3611260
Barrett Kepler <45532267+kepler-br@users.noreply.github.com>
2022-03-22 09:08:20 +0300

    Working script + README.md

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# cargo_downloader
-Crate downloader for crates.io
+# Cargo crates downloader
+
+A simple script designed for downloading crates from Rust cargo.io.  
+
+---
+
+## How to use
+
+First argument is positional argument. It specifies a path to `Cargo.lock` file and is required.
+
+| Short key | Long key        | Description                                | Type   | Required |
+|-----------|-----------------|--------------------------------------------|--------|----------|
+| -h        | --help          | Show help                                  | Flag   | No       |
+| -o        | --overwrite     | Overwrite existing crates                  | String | No       |
+| -r        | --repo          | Crates repo link                           | String | No       |
+| -O        | --output        | Output directory                           | String | No       |
+| -e        | --exit-on-error | Exit program if download error encountered | Flag   | No       |
+
+Example:
+
+```shell
+python3 crate_downloader.py Cargo.lock --output="~/CratesBareMinimumMirror"
+```   
+
+Next you'll need to fork cargo index https://github.com/rust-lang/crates.io-index and change `config.json`:
+
+```json
+{
+  "dl": "http://localhost/api/v1/crates",
+  "api": "http://localhost/"
+}
+```
+
+Push to your git and change `.cargo/.config.toml`:
+
+```toml
+[source]
+
+[source.mirror]
+registry = "https://github.com/YOUR_NAME/crates.io-index.git"
+
+[source.crates-io]
+replace-with = "mirror"
+```
+
+It's possible to use other git repositories.
---
+++ crate_downloader.py
@@ -0,0 +1,155 @@
+#!/bin/python3
+
+import os.path
+import sys
+from typing import Union
+
+import requests as requests
+from tqdm import tqdm
+import toml
+import argparse
+
+
+class RequestResult:
+    def __init__(self, content: Union[bytearray, None], status_code: int):
+        self._content = content
+        self._response_code = status_code
+
+    def get_content(self) -> bytearray:
+        return self._content
+
+    def get_status_code(self) -> int:
+        return self._response_code
+
+
+class CrateDownloadError(Exception):
+    def __init__(self, link: str, code: int):
+        self.link = link
+        self.code = code
+
+
+def get_link_with_progress(link: str, progress_text: str) -> RequestResult:
+    response = requests.get(link, stream=True)
+    total_length = response.headers.get('content-length')
+    chunk_size = 4096
+
+    if response.status_code != 200:
+        return RequestResult(content=None, status_code=response.status_code)
+
+    content = None
+    status_code = 200
+
+    if total_length is None:  # no content length header
+        progress_bar = tqdm(
+            desc=progress_text,
+            iterable=response.iter_content(chunk_size=chunk_size),
+            unit='KB',
+            bar_format='{l_bar}{bar}{r_bar:3.0f}',
+            unit_scale=1e-3,
+            leave=False, )
+
+        content = bytearray()
+
+        for data in progress_bar:
+            progress_bar.update(len(data))
+            content += data
+        progress_bar.close()
+    else:
+        total_length = int(total_length)
+        progress_bar = tqdm(
+            desc=progress_text,
+            iterable=response.iter_content(chunk_size=chunk_size),
+            total=float(total_length) + 1,
+            unit='KB',
+            bar_format='{desc}: {percentage:3.0f}%|{bar}| {n:.3f}/{total:.3f} [{elapsed}<{remaining}, '
+                       '{rate_fmt}{postfix}]',
+            unit_scale=1e-3,
+            leave=False, )
+
+        content = bytearray()
+
+        for data in progress_bar:
+            progress_bar.update(len(data))
+            content += data
+        progress_bar.close()
+
+    return RequestResult(content=content, status_code=status_code)
+
+
+def parse_arguments():
+    arg_parser = argparse.ArgumentParser(description='Mirror cargo.io crates.')
+
+    arg_parser.add_argument('cargo_lock_file', help='specify a path to a Cargo.lock',
+                            action='store', type=str)
+
+    arg_parser.add_argument('--overwrite', '-o', help='overwrite existing crates',
+                            action='store_true', required=False)
+
+    arg_parser.add_argument('--repo', '-r', help='crates repo link',
+                            action='store', type=str, default='https://crates.io',
+                            required=False, dest='repo_link')
+
+    arg_parser.add_argument('--output', '-O', help='output directory',
+                            action='store', type=str, default='./',
+                            required=False, dest='output_dir')
+
+    arg_parser.add_argument('--exit-on-error', '-e', help='exit program if download error encountered',
+                            action='store_true', required=False)
+
+    return arg_parser.parse_args()
+
+
+def download_crate(crate_io_link: str, local_path: str, name: str, version: str, overwrite_existing: bool):
+    link = os.path.join(crate_io_link, 'api/v1/crates/', name, version, 'download')
+    dir_path = os.path.join(local_path, 'api/v1/crates/', name, version)
+    crate_path = os.path.join(dir_path, 'download')
+
+    if not overwrite_existing and os.path.exists(crate_path):
+        return
+
+    result = get_link_with_progress(link, f'Downloading {name}:{version}')
+    if result.get_status_code() != 200:
+        raise CrateDownloadError(link, result.get_status_code())
+
+    os.makedirs(dir_path, exist_ok=True)
+
+    with open(crate_path, 'wb') as handle:
+        handle.write(result.get_content())
+
+
+def main():
+    args = parse_arguments()
+
+    try:
+        with open(args.cargo_lock_file, 'r') as handle:
+            cargo_lock = toml.loads(handle.read())
+    except toml.TomlDecodeError as e:
+        print(f'Error parsing {args.cargo_lock_file}:', file=sys.stderr)
+        print(f'{e}', file=sys.stderr)
+        return -1
+    except FileNotFoundError as e:
+        print(f'Error reading {args.cargo_lock_file}:', file=sys.stderr)
+        print(f'{e}', file=sys.stderr)
+        return -1
+
+    crates_progress_bar = tqdm(
+        desc=f'Crates to download',
+        iterable=cargo_lock['package'],
+        leave=False, )
+
+    for package in crates_progress_bar:
+        package_name = package['name']
+        package_version = package['version']
+        try:
+            download_crate(args.repo_link, args.output_dir, package_name, package_version, args.overwrite)
+        except CrateDownloadError as e:
+            print(f'Error downloading crate "{package_name}", version {package_version} with http code {e.code}',
+                  file=sys.stderr)
+            if args.exit_on_error:
+                return -1
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
